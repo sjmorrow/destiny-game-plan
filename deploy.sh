d98b5aea608cc0b8e9b37ea79004655462108f54
Stephen Morrow <smorrow@credera.com>
2015-06-19 00:15:26 -0500

    Second attempt at integrating travis CI

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-bundle exec rake:units
-if [ "${TRAVIS_BRANCH}" = "develop" ]; then
-	curl --ftp-create-dirs -T dist/* -u $FTP_USER:$FTP_PASSWORD ftp://ftp.dev.destinygameplan.com/home/destinygameplan/dgpdev
-fi
