fdbdbb44d0b946f972659654f6e0db0d4357c163
Sam Wilson <swilson@credera.com>
2015-06-20 16:52:05 -0500

    Added my API key and updated text on main page

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,11 +1,11 @@
 /* global malarkey:false, toastr:false, moment:false */
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('destinyGamePlan')
-    .constant('malarkey', malarkey)
-    .constant('toastr', toastr)
-    .constant('moment', moment);
-
+    angular
+        .module('destinyGamePlan')
+        .constant('malarkey', malarkey)
+        .constant('toastr', toastr)
+        .constant('moment', moment)
+        .constant('samApiKey', '5c71e24004714755b2bf2c12c9ed512b');
 })();
