e3e75d73b43ca5ff9fabde5ad15bc762942cd37a
Stephen Morrow <smorrow@credera.com>
2015-06-22 11:17:54 -0500

    Character selector WIP

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,6 +6,7 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
+<<<<<<< Updated upstream
     function MainController($timeout, bungieApi, webDevTec, toastr) {
         var vm = this;
 
@@ -36,5 +37,12 @@
                 awesomeThing.rank = Math.random();
             });
         }
+=======
+    function MainController() {
+        var vm = this;
+        vm.characters = [
+            {class:'Titan',race:'Human Male',level:34,emblem:'assets/images/characters/emblem.jpg',emblemBackground:'assets/images/characters/emblem_bg.jpg'}
+        ];
+>>>>>>> Stashed changes
     }
 })();
