25c7c74e9b71c8fe43f90ef319a2beb5f6574cbf
Sam Wilson <swilson@credera.com>
2015-06-22 11:10:01 -0500

    Updated some tab spacing

--- src/app/components/bungieApi/bungieApi.service.js
+++ src/app/components/bungieApi/bungieApi.service.js
@@ -1,32 +1,32 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('destinyGamePlan')
         .service('bungieApi', bungieApi);
-    
+
     /** @ngInject */
     function bungieApi($log, samApiKey) {
-        
+
         var baseUrl = 'https://www.bungie.net/platform/destiny/';
         var membershipType = 'tigerxbox';
         var displayName = 'xxcoastermanxx';
-        
+
         function searchDestinyPlayer() {
             $.ajax({
                 url: baseUrl + 'searchdestinyplayer/' + membershipType + '/' + displayName,
                 headers: {
-                    'X-API-Key' : samApiKey
+                    'X-API-Key': samApiKey
                 }
-            }).done(function(data){
+            }).done(function (data) {
                 $log.debug(data);
             });
         }
-        
+
         function hello() {
             $log.debug('hello');
         }
-        
+
         this.hello = hello;
         this.searchDestinyPlayer = searchDestinyPlayer;
     }
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,40 +1,40 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('destinyGamePlan')
-    .controller('MainController', MainController);
+    angular
+        .module('destinyGamePlan')
+        .controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController($timeout, bungieApi, webDevTec, toastr) {
-    var vm = this;
+    /** @ngInject */
+    function MainController($timeout, bungieApi, webDevTec, toastr) {
+        var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1434656302275;
-    vm.showToastr = showToastr;
+        vm.awesomeThings = [];
+        vm.classAnimation = '';
+        vm.creationDate = 1434656302275;
+        vm.showToastr = showToastr;
 
-    activate();
+        activate();
 
-    function activate() {
-        getWebDevTec();
-        $timeout(function() {
-            vm.classAnimation = 'rubberBand';
-        }, 4000);
-        bungieApi.searchDestinyPlayer();
+        function activate() {
+            getWebDevTec();
+            $timeout(function () {
+                vm.classAnimation = 'rubberBand';
+            }, 4000);
+            bungieApi.searchDestinyPlayer();
+        }
+
+        function showToastr() {
+            toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+            vm.classAnimation = '';
+        }
+
+        function getWebDevTec() {
+            vm.awesomeThings = webDevTec.getTec();
+
+            angular.forEach(vm.awesomeThings, function (awesomeThing) {
+                awesomeThing.rank = Math.random();
+            });
+        }
     }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
 })();
