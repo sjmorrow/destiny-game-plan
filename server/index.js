4ae53c594db6d65f25c13ac7fcb1eb870840320a
Stephen Morrow <smorrow@credera.com>
2015-06-29 13:10:06 -0500

    Updating Node server to call Bungie API

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
 var express = require('express');
-var proxy = require('express-http-proxy');
 var url = require('url');
 var cors = require('cors');
 var app = express();
+var http = require('http');
 
 app.use(cors());
 
-app.use('/bungie', proxy('www.bungie.net', {
-    forwardPath: function (req, res) {
-        return url.parse(req.url).path;
-    },
-    intercept: function(rsp, data, req, res, callback) {
-       // rsp - original response from the target
-        //res.headers['Access-Control-Allow-Origin'] = '*';
-        console.log(data.data.toJSON());
-       data = JSON.parse(data.toString('utf8'));
-       callback(null, JSON.stringify(data));
-    },
-}));
+app.get('/membershipDetails/:membershipType/:displayName', function(req, res) {
+//    var parts = url.parse(req.url, true);
+    console.log(req.params);
+    http.get('http://www.bungie.net/platform/destiny/searchDestinyPlayer/' + req.params.membershipType + '/' + req.params.displayName + '/', function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            res.json(parsed);
+        });
+    });
+})
 
 var server = app.listen(process.env.PORT || 3005, function () {
 
